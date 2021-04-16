ff701e70c0457f8a910e7c0f72064ddbf7b35b17
krhuselton <krhuselton@ualr.edu>
2021-04-16 21:36:51 +0000

    Circle Assignment

--- circle/assets/js/script.js
+++ circle/assets/js/script.js
@@ -1,24 +1,68 @@
-
 $( "#CircleForm" ).validate({
 
 });
-function displayCalculations(){
-    document.getElementById("diameter").innerHTML = "";
-    
-    var radius;
-    var diameter;
-    var circumference;
-    var area;
-    var result;
+
+function displayCalculations() {
+    if ($("#CircleForm").valid()) {
+        
+        document.getElementById("diameter").innerHTML = "";
+        
+        var radius;
+        var radiusfp;
+        var diameter;
+        
+        radius = document.getElementById("radius").value;
+        
+        radiusfp = parseFloat( radius );
+        
+        diameter = calcDiameter(radiusfp);
+        
+        document.getElementById("diameter").innerHTML = diameter.toString();
+        
+        document.getElementById("circumference").innerHTML = "";
+        
+        var circumference;
+        
+        radius = document.getElementById("radius").value;
+        
+        radiusfp = parseFloat( radius );
+        
+        circumference = calcCircumference(radiusfp);
+        
+        document.getElementById("circumference").innerHTML = circumference.toString();
+        
+        document.getElementById("area").innerHTML = "";
+        
+        var area;
+        
+        radius = document.getElementById("radius").value;
+        
+        radiusfp = parseFloat( radius );
+        
+        area = calcArea(radiusfp);
+        
+        document.getElementById("area").innerHTML = area.toString();
+        
+        var radius = prompt ("Please enter a Radius Value","Please input a Real Number")
+    }
+}
+
+function calcDiameter (radius) {
+    return Math.PI = (2 * radius);
+}
+
+function calcCircumference (radius) {
+    return 2*Math.PI * (radius);
 }
 
 function calcArea (radius) {
-    return radius * radius * math.PI;
-    
+    return Math.PI * (radius * radius);
 }
-function calcCircumference (radius) {
-    return radius * math.PI * 2;
-}
-function calcDiameter (radius) {
-    return radius * 2;
-}
+
+function clearForm() {
+            document.getElementById("radius").value = "";
+            document.getElementById("radiuserror").innerHTML = "";
+            document.getElementById("diameter").innerHTML = "";
+            document.getElementById("circumference").innerHTML = "";
+            document.getElementById("area").innerHTML = "";
+        }
